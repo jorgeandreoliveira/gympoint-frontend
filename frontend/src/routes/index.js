4bb71deebda0675d515ea907d4a9636d46a19a75
Jorge Oliveira <jorgeandreoliveira@gmail.com>
2019-11-11 22:02:18 -0300

    Commit inicial

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
+
+import Login from '../pages/Login';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Login} />
+      <Route path="/Login" component={Login} />
+    </Switch>
+  );
+}
