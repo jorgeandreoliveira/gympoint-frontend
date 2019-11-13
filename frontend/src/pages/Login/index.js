4dfbfe26370253ba811647c6ecf9e10d965cdfb7
Jorge Oliveira <jorgeandreoliveira@gmail.com>
2019-11-12 21:40:38 -0300

    Login

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-// import { Container } from './styles';
+//import { Wrapper } from '../_layouts/auth';
+
+import Logo1 from '../assets/2DF0880E-0B1E-4E3C-A8C2-3304CA0B847B@1x.png'
+import Logo2 from '../assets/223A88B0-3E04-4424-AC70-101F59035FC5@1x.png'
 
 export default function Login() {
   return (
-    <h1>Login</h1>
+    <>
+      <img src={Logo1} alt="Logo1" />
+      <img src={Logo2} alt="Logo2" />
+      <h1>GYMPOINT</h1>
+      <form>
+        <h2>SEU E-MAIL</h2>
+        <input type="email" placeholder="exemplo@email.com" />
+        <h2>SUA SENHA</h2>
+        <input type="password" placeholder="********" />
+
+        <button type="submit">Acessar</button>
+
+      </form>
+
+    </>
   );
 }
--- frontend/src/pages/_layouts/auth/styles.js
+++ frontend/src/pages/_layouts/auth/styles.js
@@ -2,5 +2,71 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: #EF4D64
+  background: #EE4D64;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 340px;
+  height: 430px;
+  text-align: center;
+  background: #ffffff;
+
+  img {
+    margin-top: 30px;
+    height: 53px;
+    width: 62px;
+  }
+
+  h1 {
+    font-size:29px;
+    margin-top: 20px;
+    color: #EE4D64;
+    font-family: 'Roboto', sans-serif;
+    font-weight: bold;
+  }
+
+  h2 {
+    text-align: left;
+    font-size: 12px;
+    padding-bottom: 5px;
+    font-family: Roboto, sans-serif;
+    padding-left: 20px;
+    color: #444;
+  }
+
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+    
+    input {
+      font-size: 16px;
+      font-family: 'Roboto';
+      border-radius: 4px;
+      border-width: 1px;
+      border-style: solid;
+      height: 44px;
+      width: 300px;
+      margin: 3px 20px 10px;
+      padding-left: 10px;      
+    }
+
+    button {
+      margin-left: 20px;
+      margin-top: 10px;
+      height: 44px;
+      width: 300px;
+      background: #EE4D64;
+      font-weight: bold;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      color: #fff;
+    }
+  }
 `;
