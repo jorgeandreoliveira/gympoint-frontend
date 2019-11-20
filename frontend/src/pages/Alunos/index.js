6627225eca7242cca8669118cbf7bb93e5f29445
Jorge Oliveira <jorgeandreoliveira@gmail.com>
2019-11-20 16:58:35 -0300

    Lista aluno style e busca aluno

--- frontend/src/pages/Alunos/index.js
+++ frontend/src/pages/Alunos/index.js
@@ -1,9 +1,111 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Content, Profile, List, LinkEditar, 
+  LinkApagar, TdCenter } from './styles';
 
-export default function Alunos() {
+let ALUNOS_INITAL_STATE = [];
+
+export default class Alunos extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      alunos: [],
+      filter: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+}
+  
+async componentDidMount() {
+
+  const response = await api.get("/students");
+
+  this.setState({
+    alunos: response.data
+  });
+
+  ALUNOS_INITAL_STATE = this.state.alunos;
+
+}
+
+renderTableHeader()
+{
   return (
-    <div/>
-  );
+  <tr>
+    <th>NOME</th>
+    <th>EMAIL</th>
+    <th>IDADE</th>
+    <th></th>
+    <th></th>
+  </tr>
+  )
+}
+
+renderTableData() {  
+  
+  return this.state.alunos.map((aluno, index) => {
+     const { id, nome, email, idade } = aluno
+     return (
+        <tr key={id}>
+           <td>{nome}</td>
+           <td>{email}</td>
+           <TdCenter>{idade}</TdCenter>
+           <td>
+             <LinkEditar to="">editar</LinkEditar>
+           </td>
+           <td>
+             <LinkApagar to="">apagar</LinkApagar>
+           </td>
+        </tr>
+     )
+  })
+}
+
+handleChange(e) {
+  
+  const filter = e.target.value;
+
+  if (filter === '')
+  {
+    this.setState({alunos: ALUNOS_INITAL_STATE});
+    return;
+  }
+
+  const listAlunos = this.state.alunos.filter(
+   aluno => aluno.nome.indexOf(filter) > -1);
+   
+   if (listAlunos.length > 0)    
+      this.setState({alunos: listAlunos});    
+    else    
+      this.setState({alunos: ALUNOS_INITAL_STATE});
+  }
+
+  render () {
+
+    return(
+      <Container>
+      <Content>
+      <nav>
+        <h1>Gerenciando alunos</h1>        
+        </nav>
+        <aside>
+          <Profile>
+            <div>
+              <button>+ CADASTRAR</button>
+              <input onChange={this.handleChange} placeholder="Buscar aluno"/> 
+            </div>
+          </Profile>
+        </aside>        
+      </Content>
+      <List>        
+        <tbody>
+          {this.renderTableHeader()}
+          {this.renderTableData()}
+        </tbody>
+      </List>      
+    </Container>
+    )
+  }
 }
---
+++ frontend/src/pages/Alunos/styles.js
@@ -0,0 +1,87 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Container = styled.div`
+  height:100px;
+`;
+
+export const Content = styled.div`
+  height: 64px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: 120px;
+
+  nav {
+    display: flex;
+    align-items: center;
+    
+    h1 {
+      color: #444;
+      font-size: 24px;
+      margin-right: 10px;
+      padding-right: 50px;
+    }
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  margin-left: 10px;
+  padding-left: 10px;
+  align-items: center;
+  justify-content: center;  
+
+  div {
+    text-align: right;
+    margin-right: 100px;
+    
+    button {
+      height: 36px;
+      width: 142px;
+      border: 0;
+      border-radius: 4px;
+      background: #EE4D64;
+      font-weight: bold;
+      
+      padding-right: 10px;
+      color: #fff;
+    }
+
+    input {
+      width: 230px;
+      height: 36px;
+      padding-left: 10px;
+      margin-left: 10px;
+    }
+  }`;
+
+export const TdCenter = styled.td`
+
+  text-align: center;
+`;
+
+export const List = styled.table`
+  
+  th, td {
+    padding: 0 0 8px 150px;
+  }
+  th {
+    text-align: left;
+  }
+  td {
+    font-size: 16px;    
+  }
+`
+export const LinkEditar = styled(Link)`
+  font-size:15px;
+  color:#4d85ee;
+ 
+`
+
+export const LinkApagar = styled(Link)`
+  font-size:15px;
+  color:#de3b3b;
+ 
+`
